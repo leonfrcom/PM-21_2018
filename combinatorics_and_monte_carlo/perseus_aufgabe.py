095308030c95b51d9f24682b0f33a31a26a389f8
leonfrcom <38557294+leonfrcom@users.noreply.github.com>
2018-06-06 09:45:41 +0000

    Update perseus_aufgabe.py
    Das Programm liefert die entgegengesetzte Lösung...

--- combinatorics_and_monte_carlo/perseus_aufgabe.py
+++ combinatorics_and_monte_carlo/perseus_aufgabe.py
@@ -5,17 +5,51 @@
 
 import random
 
-# set up the three caves
-caves = [1,2,3]
-# initialize counters for the two possible outcomes
+
 counter_andromeda_in_original_cave = 0
 counter_andromeda_in_third_cave = 0
 
+def monster_func():
+    kein_monster = random.choice(caves)
+    monster_liste = caves[:]
+    monster_liste.remove(kein_monster)
+    return monster_liste
+
+def perseus_wahl():
+    wahl = random.choice(caves)
+    return wahl
+
+def tipp_func(wahl, monster):
+    if wahl in monster:
+        monster.remove(wahl)
+        return monster[0]
+    else:
+        n = random.randint(0, 1)
+        return monster[n]
+
+def perseus_wahl_2(tipp, wahl_1):
+    caves.remove(tipp)
+    wahl = random.choice(caves)
+    if wahl == wahl_1:
+        return wahl, '0'
+    else:
+        return wahl, '1'
+
+
 trials = 10000
 for trial in range(trials):
-    # simulate and count outcomes
-    # TO DO!!
-    pass
+    caves = [1,2,3]
+    auswahl = perseus_wahl()
+    monster = monster_func()
+    tipp = tipp_func(auswahl, monster)
+    wahl_2, wert = perseus_wahl_2(tipp, auswahl)
+
+    if wert == '1':
+        if wahl_2 not in monster:
+            counter_andromeda_in_third_cave += 1
+            
+            
+            
 
 # report results of simulation
 print(
@@ -23,4 +57,3 @@ print(
     '{0} out of {1} cases.'
     .format(counter_andromeda_in_third_cave, trials)
     )
-    
